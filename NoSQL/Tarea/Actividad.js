aa6c913441218977ac0b3fcb534f792c28a8bc04
gerardofv02 <gerardofv02@gmail.com>
2026-04-02 20:28:15 +0200

    Fin tarea NOSQL

--- NoSQL/Tarea/Actividad.js
+++ NoSQL/Tarea/Actividad.js
@@ -67,10 +67,119 @@ db.Movies.aggregate([
 /* Fin Ejercicio 13 */
 
 /* Comienzo Ejercicio 14 */
-db.Movies.aggregate([
-    {$match : {$and: [{'year': {$gte: 1960}}, {'year': {$lte: 1969}}]}},
+var year_most_total = db.Movies.aggregate([
     { $group: {'_id': '$year', 'total' : {$sum: 1}}}
-    ]).sort({'_id': 1})
+    ]).sort({'total': -1}).limit(1).toArray()
+//una vez sacamos el total más alto, buscamos por este valor:
+var total_maximo = year_most_total[0].total
+total_maximo
+db.Movies.aggregate([
+    { $group: {'_id': '$year', 'total' : {$sum: 1}}},
+    {$match : {'total': {$eq : total_maximo}}}
+    ]).sort({'total': 1})
 /* Fin Ejercicio 14 */
 
+/* Comienzo Ejercicio 15 */
+var year_least_total = db.Movies.aggregate([
+    { $group: {'_id': '$year', 'total' : {$sum: 1}}}
+    ]).sort({'total': 1}).limit(1).toArray()
+//una vez sacamos el total más alto, buscamos por este valor:
+var total_minimo = year_least_total[0].total
+total_minimo
+db.Movies.aggregate([
+    { $group: {'_id': '$year', 'total' : {$sum: 1}}},
+    {$match : {'total': {$eq : total_minimo}}}
+    ]).sort({'total': 1})
+/* Fin Ejercicio 15 */
 
+/* Comienzo Ejercicio 16 */
+var fase_unwind = {$unwind: '$cast'}
+var query_no_repetidos = {'_id': 0} //quitamos el id para no generar errores de duplicados
+var fase_no_repetidos = { $project: query_no_repetidos}
+var fase_out = {$out: 'actors'}
+var etapas = [fase_unwind,fase_no_repetidos,fase_out]
+db.Movies.aggregate(etapas)
+//probamos y contamos
+db.actors.find().count()
+/* Fin Ejercicio 16 */
+
+/* Comienzo Ejercicio 17 */
+db.actors.aggregate([
+    {$match: {'cast': {$ne: 'Undefined'}}},
+    {$group: {'_id': '$cast', 'total_pelis': {$sum:1}}}
+    ]).sort({'total_pelis': -1}).limit(5)
+/* Fin Ejercicio 17 */
+
+/* Comienzo Ejercicio 18 */
+db.actors.aggregate([
+    {$group: {'_id': {'title': '$title','year': '$year'}, 'total_actors': {$sum:1}}}
+    ]).sort({'total_actors': -1}).limit(5)
+/* Fin Ejercicio 18 */
+
+/* Comienzo Ejercicio 19 */
+db.actors.aggregate([
+    {$match: {'cast': {$ne: 'Undefined'}}},
+    {$group: {'_id': '$cast','comienza': {$min: '$year'},'termina':{$max: '$year'}}},
+    {$addFields:{'años':{ $subtract: ['$termina','$comienza']}}}
+    ]).sort({'años': -1}).limit(5)
+/* Fin Ejercicio 19 */
+
+/* Comienzo Ejercicio 20 */
+var fase_unwind = {$unwind: '$genres'}
+var query_no_repetidos = {'_id': 0} //quitamos el id para no generar errores de duplicados
+var fase_no_repetidos = { $project: query_no_repetidos}
+var fase_out = {$out: 'genres'}
+var etapas = [fase_unwind,fase_no_repetidos,fase_out]
+db.actors.aggregate(etapas)
+//probamos y contamos
+db.genres.find().count()
+/* Fin Ejercicio 20 */
+
+/* Comienzo Ejercicio 21 */
+db.genres.aggregate([
+    {$match: {'genres': {$ne: 'Undefined'}}},
+    {$group: {'_id': {'generos': '$genres', 'year': '$year'}, 'total_pelis': {$sum: 1}}}
+    ]).sort({'total_pelis': -1}).limit(5)
+/* Fin Ejercicio 21 */
+
+/* Comienzo Ejercicio 22 */
+db.genres.aggregate([
+    {$match: {'cast': {$ne: 'Undefined'}}},
+    {$group: {'_id': '$cast', 'generos': {$addToSet: '$genres'}}},
+    {$addFields:{'numgeneros': {$size: '$generos'}}}
+    ]).sort({'numgeneros': -1}).limit(5)
+/* Fin Ejercicio 22 */
+
+/* Comienzo Ejercicio 23 */
+db.genres.aggregate([
+    {$match: {'genres': {$ne: 'Undefined'}}},
+    {$group: {'_id': {'title': '$title', 'year': '$year'}, 'generos': {$addToSet: '$genres'}}},
+    {$addFields:{'numgeneros': {$size: '$generos'}}}
+    ]).sort({'numgeneros': -1}).limit(5)
+/* Fin Ejercicio 23 */
+
+/* Comienzo Ejercicio 24 */
+db.genres.aggregate([
+    {$match: {'genres': {$ne: 'Undefined'}}},
+    {$group: {'_id': '$genres', 'peliculas': {$addToSet: '$title'}}},
+    {$addFields: {'total_pelis': {$size: '$peliculas'}}}
+    ]).sort({'total_pelis': -1}).limit(5)
+/* Fin Ejercicio 24 */
+
+/* Comienzo Ejercicio 25 */
+db.genres.aggregate([
+    {$match: {'cast': {$ne: 'Undefined'}, 'genres': {$ne: 'Undefined'}}},
+    {$group: {'_id': '$cast', 'generos': {$addToSet: '$genres'}}},
+    {$match: {$expr: {$eq: [{$size: '$generos'}, 1]}}},
+    {$addFields:{'numgeneros': {$size: '$generos'}}}
+    ]).sort({'numgeneros': -1})
+/* Fin Ejercicio 25 */
+
+/* Comienzo Ejercicio 26 */
+db.actors.aggregate([
+    {$match: {'cast': {$ne: 'Undefined'}}},
+    {$group: {'_id': '$cast','peliculas': {$addToSet: '$title'}}},
+    {$addFields: {'total_pelis': {$size: '$peliculas'}}},
+    {$match: {'total_pelis' : {$gt: 10}}}
+    ]).sort({'total_pelis': 1})
+/* Fin Ejercicio 26 */
